b0109cd43e726a3c895aa88c244286e79ecd63e2
kovacsrud <kovacs.rud@gmail.com>
2025-04-11 12:45:39 +0200

    Alap React app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center text-sky-800">Git gyakorlás</h1>
+      <h2 className="text-xl">Tailwind és DaisyUI telepítve</h2>
+    </div>
+  )
+}
+
+export default App
